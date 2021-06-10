72fcaa45d3d1a0b81a9cdca470f388a6fba00d63
deepyes02 <deepyes@outlook.com>
2021-06-11 00:49:30 +0545

    Get, update, delete and post API Sucessfully built

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Exception;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resources
+     */
+    public function index()
+    {
+        $product = Product::all();
+        if(count($product) < 1)
+        {
+            return [
+            "message" => "No products exist, add products first"
+        ];
+        }
+        else {
+            return $product;
+        }
+    }
+
+    /**
+     * Post Request, store products
+     *
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+        'name'  => 'required',
+        'slug'  =>  'required',
+        'description'   => 'required',
+            'price'     => 'required'
+        ]);
+        try {
+            return Product::create($request->all());
+        } catch (Exception $e) {
+            return [
+                "message" => $e->getMessage()
+            ];
+        }
+    }
+
+    /**
+     * Get Products by Id
+     */
+    public function show($id) {
+        if (Product::find($id)) {
+            return Product::find($id);
+        } else {
+            return [
+                "message" => "Product not found"
+            ];
+        }
+    }
+
+    /**
+     * Update Product by Id
+     */
+    public function update(Request $request , $id)
+    {
+        $product = Product::find($id);
+        if($product !== null)
+        {
+            $product->update($request->all());
+            return $product;
+        } else {
+            return [
+                "message"   => "sorry product with id {$id} not found"
+            ];
+        }
+
+    }
+
+    /**
+     * Delete Product
+     */
+
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        if ($product !== null)
+        {
+            $product->delete();
+            return [
+                "message"   => "Product {$id} destroyed successfully"
+            ];
+        }
+        else return [
+            "message" => "Sorry product with id '{$id}' not found"
+        ];
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('products', [ProductController::class, 'index'])->name('products');
+Route::post('products', [ProductController::class, 'store']);
+
+Route::get('products/{id}', [ProductController::class, 'show']);
+Route::put('products/{id}', [ProductController::class, 'update']);
+
+Route::delete('products/{id}', [ProductController::class, 'destroy']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
