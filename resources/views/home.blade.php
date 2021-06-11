8b45f961109046c12f40ad20c7ab35a163b51c8f
deepyes02 <deepyes@outlook.com>
2021-06-11 11:47:06 +0545

    after installing vue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,11 +18,11 @@
                 font-family: 'Segoe UI', sans-serif;
             }
         </style>
+
     </head>
     <body>
-    <div class="my-5 p-6 max-w-sm mx-auto bg-blue-50 rounded-xl shadow-md flex items-center space-x-4">
+    <div class="my-5 p-6 max-w-sm mx-auto bg-blue-100 rounded-xl shadow-md flex items-center space-x-4">
         <div class="flex-shrink-0">
-{{--            <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">--}}
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
             </svg>
