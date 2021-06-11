12e6b36b62d2da2816e81fa511552d88ee2ec424
deepyes02 <deepyes@outlook.com>
2021-06-11 11:31:03 +0545

    installed tailwind css

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Home</title>
+
+        <!-- Fonts -->
+{{--        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">--}}
+
+        <!-- styling -->
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+
+        <style>
+            body {
+                font-family: 'Segoe UI', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+    <div class="my-5 p-6 max-w-sm mx-auto bg-blue-50 rounded-xl shadow-md flex items-center space-x-4">
+        <div class="flex-shrink-0">
+{{--            <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">--}}
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+            </svg>
+        </div>
+        <div>
+            <div class="text-xl font-medium text-black">ChitChat</div>
+            <p class="text-gray-500">You have a new message!</p>
+        </div>
+    </div>
+
+
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/default', function () {
     return view('welcome');
 });
+
+Route::get('/', fn()=> view('home'));
